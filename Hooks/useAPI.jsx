b6e67f0716f1a203faf2c3d7144c925ae5999162
Saaransh Sharma <saaranshsharma28@gmail.com>
2023-11-21 12:12:35 -0500

    making it even better

--- Hooks/useAPI.jsx
+++ Hooks/useAPI.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
-import {API_SERVER} from '@env'
+import { Linking } from "react-native";
 
 const useAPI = () => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const scanStudent = async (seatNumber) =>{
+
+    const scanStudent = async (seatNumber) => {
         setLoading(true);
         setError(null);
 
+        const url = await Linking.getInitialURL();
+        const ipAddressRegex = /\/\/(.*):/; // Regular expression to extract IP address
+        const match = ipAddressRegex.exec(url);
+        const ipAddress = match[1];
+
         console.log(` seat number: ${seatNumber}`)
-        const response = await fetch(`${API_SERVER}/api/studentScan/`, {
-            method:'POST',
-            headers:{
+
+        const response = await fetch(`http://${ipAddress}:3000/api/studentScan/`, {
+            method: 'POST',
+            headers: {
                 "Content-Type": "application/json"
             },
-            body:JSON.stringify({
-                "studentID":100701820,
+            body: JSON.stringify({
+                "studentID": 100701820,
                 "seatNumber": seatNumber,
                 "roomNumber": "UA1350"
             })
@@ -25,7 +32,7 @@ const useAPI = () => {
 
         const json = await response.json();
 
-        if(!response.ok){
+        if (!response.ok) {
             setLoading(false)
             setError(json.error)
         }
